8557963bb97e332a8900f4159158ba0c9c18ab0a
tywalker <tywalker_@outlook.com>
2019-10-01 12:25:48 +0000

    Correct answer with one pass hash

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,22 +6,19 @@ object Main extends App {
   val nums: List[Int] = List(4,5,7,3,2,9)
   val expectation = 10
 
-  def makeMap(nums: List[Int], sum: Int) = {
+  def twoSum(nums: List[Int], sum: Int) = {
     var numMap = Map.empty[Int, Int]
     var answer = Map.empty[Int, Int]
     for (num <- nums) {
       numMap(sum - num) = num
-      numMap.retain((k,v) => numMap.contains(v) && k != v)
-      if (answer.size != 0) answer
+      if (numMap.contains(num) && (sum - num) != num) {
+        answer(num) = numMap(num)
+      }
+
     }
     answer
   }
 
-  def twoSum(numMap: Map[Int, Int]) = {
-    numMap.retain((k,v) => numMap.contains(v) && k != v)
-  }
-
-  val map = makeMap(nums, 10)
-  // val a = twoSum(map)
-  println(map)
+  val answer = twoSum(nums, 10)
+  println(answer)
 }
