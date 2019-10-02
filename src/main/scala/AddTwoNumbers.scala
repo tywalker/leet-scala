a9502e4bf5367a31e2ea804b7a93e6f34eb7f35e
Tyler Walker <jtwalker@ebsco.com>
2019-10-02 08:14:24 -0400

    wrote code for first and insert ll

--- src/main/scala/AddTwoNumbers.scala
+++ src/main/scala/AddTwoNumbers.scala
@@ -1,17 +1,37 @@
 package addTwoNumbers
 
+trait NodeItem {
+  def data: Int
+}
+
+class Node(data: Int) {
+  var data: Int = data
+  var next: NodeItem = null
+}
+
+class List {
+  var head = null
+
+  def insert(data: Int) = {
+    val node = new Node(data)
+    node.next = head
+    head = node
+  }
+}
+
 class AddTwoNumbers {
+  type Linked = Map[Int, Node]
   // create ListNode
   // create LinkedList
-  def createNode(data: Int) = {
-    val node = Map("data" -> data, "next" -> -1)
-    // id
-    // data
-    // next
-
-  }
-
+  // first is f(x) where x == head
+  // first is f(next(x)) where x == head next
+  // until next == null
   def init() = {
-    println("working")
+    val list = new List();
+
+    list.insert(0)
+    list.insert(3)
+
+    print(list.head)
   }
 }
